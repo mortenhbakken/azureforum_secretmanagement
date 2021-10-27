91638c0d40df725693a33843dc307b84eced6c2c
Morten Hordnes Bakken <morten.h.bakken@visma.com>
2021-10-27 16:17:37 +0200

    inital setup

---
+++ website/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace azureforum_secretmanagement.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+        public string Secret { get; set; }     
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public void OnGet()
+        {
+            var secret = _config["SecretValue"];
+            this.Secret = secret;
+        }
+    }
+}
